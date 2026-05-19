d760b7015afdc14ad3052058d367b89bb3e6315d
goolbi <pinpinprin1227@gmail.com>
2026-05-19 23:30:46 +0900

    test11111
    1233213213213

--- Assets/213123213.cs
+++ Assets/213123213.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class 123213sadas : MonoBehaviour
+public class 213123213 : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
